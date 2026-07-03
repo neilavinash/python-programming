f47da2559fc02b41dbaf08af0e2e2d46fb631422
Neil Avinash <neil.avinash16@gmail.com>
2026-07-03 10:38:34 -0400

    test

--- 30-Jun-2026/main.py
+++ 30-Jun-2026/main.py
@@ -1,13 +1,6 @@
-#animal = input ("what is your favorite animal")
-#print (animal)
-#country = input ("what is your favorite country")
-#print (country)
-#candy = input ("what is your favorite candy")
-#print (candy)
-#fruit = input ("what is your favorite fruit")
-#print (fruit)
-#food = input("what is your favorite food")
-#print (food)
+animal = input ("what is your favorite animal")
+print (animal)
+asda
  
 
 
