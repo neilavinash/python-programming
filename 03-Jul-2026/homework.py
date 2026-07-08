2693d6f56f63d1f24216e1bd211a38caf82d4290
Neil Avinash <neil.avinash16@gmail.com>
2026-07-07 22:04:51 -0400

    homework123

--- 03-Jul-2026/homework.py
+++ 03-Jul-2026/homework.py
@@ -1,12 +1,1 @@
-
-a=22
-b=float(a)
-print(b)
-
-x=65.4
-y=int(x)
-print(y)
-
-x=40j
-print(type(x))
-print(x+y)
+print(len("pancake"))
--- 03-Jul-2026/homework1.py
+++ 03-Jul-2026/homework1.py
@@ -1,13 +1,1 @@
-
-a=44
-b=float(a)
-print(b)
-
-x=53.5
-y=int(x)
-print(y)
-
-
-x=80j
-print(type(x))
-print(x+y)
+print(len("ice cream"))
